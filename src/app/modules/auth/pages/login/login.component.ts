4ba756ee3082440bad375b7088d25e685dc4f839
Geraldine Gómez <geralgomez1403@gmail.com>
2024-04-01 23:14:01 -0500

    [feat] Registration and login functionality added

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-  constructor(private readonly fb: FormBuilder) {}
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor(
+    private readonly fb: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   public loginForm!: FormGroup;
+  public errorSession!: boolean;
+  private subscription!: Subscription;
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
     this.loginForm = this.initForm();
+
+    this.subscription = this.authService.errorLogin$.subscribe((error) => {
+      this.errorSession = error;
+    });
   }
 
   initForm(): FormGroup {
@@ -28,11 +39,23 @@ export class LoginComponent implements OnInit {
     });
   }
 
+  onSubmit(): void {
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password);
+  }
+
   get email(): AbstractControl | null {
     return this.loginForm.get('email');
   }
 
   get password(): AbstractControl | null {
     return this.loginForm.get('password');
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.authService.resetErrorSession();
   }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
